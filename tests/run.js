9d03843e0b037253dae79e4963b4e5d2de077836
Sean Wernimont <swernimo@users.noreply.github.com>
2021-02-08 20:20:37 -0700

    added empty run function

---
+++ tests/run.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const checkPackageJson = require('./checkPackageJson');
+
+const run = function() {
+    console.log('======running toffeenut ==========');
+    /**
+     * load config
+     * 
+     */
+}
+
+module.exports = run;
