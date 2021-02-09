c215d5528874032f17e18b0f7f08efed31bb9d45
Sean Wernimont <swernimo@users.noreply.github.com>
2021-02-08 20:57:41 -0700

    bug fix creating config file

--- tests/init.js
+++ tests/init.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 const init = function() {
     fs.writeFileSync('./toffeenut.config.json', '{ \r\n\
        "checkPackageJson": { \r\n\
-        "enabled": false, \r\n\
-       \t}, \r\n\
+        "enabled": false \r\n\
+       \t\t}, \r\n\
        "singleExport": { \r\n\
         "allowInterfaces" : true, \r\n\
-        "enabled": true, \r\n\
-        \t}\r\n \
+        "enabled": true \r\n\
+        \t\t}\r\n \
     }');
 }
 
