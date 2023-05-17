fb938369c19d31100485ab8a173fe29146665f3c
Sean Wernimont <swernimo@users.noreply.github.com>
2023-05-16 20:47:55 -0600

    bug fix in init for hex colors

--- scripts/init.js
+++ scripts/init.js
@@ -11,12 +11,12 @@ const init = function() {
         \t},\r\n \
         "hexColors": { \r\n\
             \t"rootPath" : \"\", \r\n\
-            \t"enabled": true \r\n\
+            \t"enabled": true, \r\n\
             \t"checkHTML" : false, \r\n\
-            \t"checkForRGBA": false \r\n\
+            \t"checkForRGBA": false, \r\n\
             \t"ignoreDirectory" : \"\", \r\n\
             \t"colorsFilePath": \"\" \r\n\
-        \t},\r\n \
+        },\r\n \
     }');
 }
 
