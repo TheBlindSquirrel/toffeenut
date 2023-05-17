8f801783d9546b2f0599c624322a905cfb63a22a
Sean Wernimont <swernimo@users.noreply.github.com>
2023-05-17 15:20:01 -0600

    ignoring font files

--- scripts/hexColors.js
+++ scripts/hexColors.js
@@ -90,7 +90,7 @@ function getAllFiles(path, ignoreHtml) {
         }
         else {
             const fileExt = Path.extname(absolute);
-            const ignoreFileExts = ['.ts'];
+            const ignoreFileExts = ['.ts', '.eot', '.svg', '.ttf', '.woff'];
             if (ignoreHtml) {
                 ignoreFileExts.push('.html');
                 ignoreFileExts.push('.htm');
